5f606009339323be82be9401c2b7993850062655
zaqijr7 <zaqijr7@gmail.com>
2021-04-03 20:30:27 +0700

    change flow auth

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -11,8 +11,34 @@ exports.register = async (req, res) => {
   const { email, phoneNumber } = req.body
   try {
     const emailExist = await userModel.getUsersByConditionAsync({ email })
+    console.log(emailExist, '<<<<<<<< ini email')
     if (emailExist.length > 0) {
-      return responseStatus.emailIsExist(res)
+      const lengthPin = 5
+      const pin = Math.floor(Math.pow(10, lengthPin - 1) + Math.random() * 9 * Math.pow(10, lengthPin - 1))
+      const salt = await bcrypt.genSalt()
+      const encryptedPin = await bcrypt.hash(pin.toString(), salt)
+      const updatePin = await userModel.updateProfile(emailExist[0].id_user, { pin: encryptedPin })
+      console.log(updatePin)
+      const data = {
+        email: email,
+        subject: 'Pin Verification',
+        html: ` <div>
+      <h1>Pin Verification</h1>
+      <h3>${pin}<h3>
+      </div>`
+      }
+      sendEmail(data)
+      const result = await userModel.getUserById(emailExist[0].id_user)
+      console.log(result, 'ini data user')
+      const dataUser = {
+        email: result[0].email,
+        phoneNumber: result[0].phoneNumber
+      }
+      return res.status(200).json({
+        success: true,
+        message: 'Please check email to get PIN verification',
+        result: dataUser
+      })
     }
     const lengthPin = 5
     const pin = Math.floor(Math.pow(10, lengthPin - 1) + Math.random() * 9 * Math.pow(10, lengthPin - 1))
@@ -26,20 +52,18 @@ exports.register = async (req, res) => {
         subject: 'Pin Verification',
         html: ` <div>
       <h1>Pin Verification</h1>
-      <h5>${pin}<h5>
+      <h3>${pin}<h3>
       </div>`
       }
       sendEmail(data)
       const result = await userModel.getUserById(createUser.insertId)
-      console.log(result, 'ini data user')
       const dataUser = {
         email: result[0].email,
         phoneNumber: result[0].phoneNumber
       }
-      console.log(dataUser)
       return res.status(200).json({
         success: true,
-        message: 'Register Successfully, please check email to get PIN verification',
+        message: 'Please check email to get PIN verification',
         result: dataUser
       })
     }
@@ -74,7 +98,7 @@ exports.login = async (req, res) => {
     }
     return res.status(404).json({
       success: false,
-      message: 'Email, phone number or PIN is Wrong'
+      message: 'PIN is Wrong'
     })
   } catch (error) {
     return res.status(500).json({
